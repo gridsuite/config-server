b5944f6258890f3cca3827ef1356388bae88b8b1
Seddik Yengui <seddik.yengui@rte-france.com>
2022-09-15 15:44:39 +0200

    add license and author

--- src/main/java/org/gridsuite/config/server/service/NotificationService.java
+++ src/main/java/org/gridsuite/config/server/service/NotificationService.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package org.gridsuite.config.server.service;
 
 import org.gridsuite.config.server.ConfigService;
@@ -8,6 +15,10 @@ import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
+
+/**
+ * @author Seddik Yengui <seddik.yengui at rte-france.com>
+ */
 
 @Service
 public class NotificationService {
