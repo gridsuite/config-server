1502a7eceb0b77f8a59d19ccc3f629fb6c14b93b
sBouzols <sylvain.bouzols@gmail.com>
2023-11-22 16:46:49 +0100

    update to gridsuite dependencies 28
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/config/server/ConfigApplication.java
+++ src/main/java/org/gridsuite/config/server/ConfigApplication.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.config.server;
 
-import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -21,7 +20,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 public class ConfigApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(ConfigApplication.class, args);
     }
 
