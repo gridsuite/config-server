40565ede443e3919683a57ff72c8c3fd137fd9c3
Jon Harper <jon.harper87@gmail.com>
2026-06-23 11:48:27 +0200

    checkstyleparent27 (#102)
    Signed-off-by: HARPER Jon <jon.harper87@gmail.com>
    Co-authored-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/main/java/org/gridsuite/config/server/repository/ParameterEntity.java
+++ src/main/java/org/gridsuite/config/server/repository/ParameterEntity.java
@@ -6,16 +6,15 @@
  */
 package org.gridsuite.config.server.repository;
 
-import java.util.UUID;
-
 import lombok.Getter;
 import lombok.Setter;
 import org.gridsuite.config.server.dto.ParameterInfos;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
-import org.springframework.data.domain.Persistable;
+import java.util.UUID;
 
 /**
  * @author Abdelsalem Hedhili <abdelsalem.hedhili at rte-france.com>
